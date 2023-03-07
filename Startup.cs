6a51a74a4a4878152e456fe99ba3ef621bdb9116
Anthony <anthonykfho@gmail.com>
2023-03-07 11:27:15 -0500

    init
    - project template files created

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hospital_project.Startup))]
+namespace hospital_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
