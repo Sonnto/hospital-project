043e9d3022f5744a67f52a2999eee39209657db1
Anthony <anthonykfho@gmail.com>
2023-04-12 12:31:21 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/Sonnto/hospital-project""
    This reverts commit 3f19fc23e6bfcab0b8420c56421f88e9538600f8.

--- Models/Department.cs
+++ Models/Department.cs
@@ -15,4 +15,13 @@ namespace hospital_project.Models
         //A department can have multiple physicians associated with them
         public ICollection<Physician> Physicians{ get; set; }
     }
+
+    public class DepartmentDto
+    {
+        public int department_id { get; set; }
+        public string department_name { get; set; }
+
+        //A department can have multiple physicians associated with them
+        public ICollection<Physician> Physicians { get; set; }
+    }
 }
